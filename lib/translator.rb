d580ae118c5df1346f8a2ef192bc3eb01c24685a
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-06 21:56:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(emoticon_file)
     japanese = { japanese: value[1]}
     new_hash = english.merge(japanese)
     value.each do |keys|
-      final_array
+      
     binding.pry
   end
   binding.pry
